c62d4961e3e817230e9c75f409467bbec0e3e2ff
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2024-02-20 15:38:54 +0100

    working on progress

--- src/components/Results/Statistics.jsx
+++ src/components/Results/Statistics.jsx
@@ -1,39 +1,21 @@
+import { getAmounts, getStat } from "../../util/statistics";
+
 export default function Statistics({ theResults }) {
-  function getStat(amount, total) {
-    const stat = (amount * 100) / total;
-    return Math.floor(stat);
-  }
-
-  const statistics = theResults.reduce(
-    (acc, obj) => {
-      if (obj.userAnswer.result === true) {
-        acc.true += 1;
-      } else if (obj.userAnswer.result === false) {
-        acc.false += 1;
-      }
-      return acc;
-    },
-    { true: 0, false: 0 }
-  );
-
-  console.log(statistics);
+  const total = theResults.length;
+  const statistics = getAmounts(theResults);
 
   return (
     <ol id="summary-stats">
       <li>
-        <div className="number">70%</div>
+        <div className="number">99%</div>
         <div className="text">skipped</div>
       </li>
       <li>
-        <div className="number">
-          {getStat(statistics.true, theResults.length)}%
-        </div>
+        <div className="number">{getStat(statistics.true, total)}%</div>
         <div className="text">answered correctly</div>
       </li>
       <li>
-        <div className="number">
-          {getStat(statistics.false, theResults.length)}%
-        </div>
+        <div className="number">{getStat(statistics.false, total)}%</div>
         <div className="text">answered incorrectly</div>
       </li>
     </ol>
---
+++ src/util/statistics.js
@@ -0,0 +1,19 @@
+export function getAmounts(array) {
+  const statistics = array.reduce(
+    (acc, obj) => {
+      if (obj.userAnswer.result === true) {
+        acc.true += 1;
+      } else if (obj.userAnswer.result === false) {
+        acc.false += 1;
+      }
+      return acc;
+    },
+    { true: 0, false: 0 }
+  );
+  return statistics;
+}
+
+export function getStat(amount, total) {
+  const stat = (amount * 100) / total;
+  return Math.floor(stat);
+}

0c1deb43c91548c2d85497920bd9832bd9a5fc28
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2024-02-21 14:24:37 +0100

    working on timing selected option

--- src/components/Questions/Option.jsx
+++ src/components/Questions/Option.jsx
@@ -2,11 +2,13 @@ import { useContext } from "react";
 import { QuizContext } from "../../store/quiz-context";
 
 export default function Option({ textAnswer }) {
-  const { handleQuestionChangeCtx, currentQuestionCtx } =
-    useContext(QuizContext);
+  const { handleQuestionChangeCtx } = useContext(QuizContext);
   return (
     <li className="answer">
-      <button onClick={() => handleQuestionChangeCtx(textAnswer)}>
+      <button
+        // className="selected"
+        onClick={() => handleQuestionChangeCtx(textAnswer)}
+      >
         {textAnswer.answer}
       </button>
     </li>
